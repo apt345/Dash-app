40602ccef95da7494e3ca5b2196e73f0d2ed076e
Guillermo Hernández <g.hernandezretuerto@gmail.com>
2021-03-23 15:18:08 +0100

    Added Guillermo's processing and checkbox

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import dash
 from dash.dependencies import Input, Output, State
-import math
 import dash_html_components as html
 import dash_core_components as dcc
 import plotly.express as px
@@ -12,8 +11,9 @@ app = dash.Dash(__name__, title="Arturo and Guille Dash app")
 import numpy as np
 import pandas as pd
 import json
+import math
 
-
+#######ARTURO########
 #loading the covid dataset
 df_url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv'
 df = pd.read_csv(df_url).dropna(subset = ['location'])
@@ -33,32 +33,71 @@ nrows=len(df_dates)
 min_date = df_dates[0]
 max_date = df_dates[nrows-1]
 selected_dates = [min_date, df_dates[math.floor(nrows/10)], df_dates[math.floor(nrows*2/10)], df_dates[math.floor(nrows*3/10)], df_dates[math.floor(nrows*4/10)], df_dates[math.floor(nrows*5/10)], df_dates[math.floor(nrows*6/10)], df_dates[math.floor(nrows*7/10)], df_dates[math.floor(nrows*8/10)], df_dates[math.floor(nrows*9/10)], max_date]
+############################
 
+###GUILLERMO################
+# Load the data and select the columns
+data = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data')
+data.columns = ['age','workclass','fnlwgt','education','education_num','marital_status','occupation','relationship','race','sex','cap_gain','cap_loss','hours_week','native_country','label']
+data = data[['age','workclass','education','race','sex','hours_week','label']]
 
+# Drops missing values (marked as ' ?' in the dataframe)
+data = data[~data.eq(' ?').any(1)]
+#data.isna().sum()
+
+# Modify the categories
+data['workclass'] = data['workclass'].replace('.*^\\ Self.*', 'Self Employed', regex=True)
+data['workclass'] = data['workclass'].replace([' Local-gov', ' State-gov', ' Federal-gov'], 'Public', regex=True)
+data['workclass'] = data['workclass'].replace(' Without-pay', 'Without Pay', regex=True)
+data['workclass'] = data['workclass'].replace(' Never-worked', 'Never Worked', regex=True)
+data['workclass'] = data['workclass'].replace(' Private', 'Private', regex=True)
+
+data['education'] = data['education'].replace(' 1st-4th', 'Elementary School', regex=True)
+data['education'] = data['education'].replace([' 5th-6th',' 7th-8th'], 'Middle School', regex=True)
+data['education'] = data['education'].replace([' 9th',' 10th',' 11th',' 12th'], 'High School', regex=True)
+data['education'] = data['education'].replace(' Some-college', 'Unfinnished College', regex=True)
+data['education'] = data['education'].replace(' Assoc-voc', 'Assoc', regex=True)
+data['education'] = data['education'].replace(' Assoc-acdm', 'Assoc', regex=True)
+
+data['race'] = data['race'].replace(' Amer-Indian-Eskimo', 'Ind-Eskimo', regex=True)
+data['race'] = data['race'].replace(' Asian-Pac-Islander', 'Asian-Pac', regex=True)
+
+# Convert categorical variables into 'category'
+data[['workclass','education','race','sex','label']] = data[['workclass','education','race','sex','label']].astype("category")
+
+# Convert label into (0,1)
+data['label'] = np.where(data['label'] == ' >50K', 1, 0)
+
+#########################################3
 
 # Selection for Guille
-
-df_guille = [0,1]
-opt_guille = [{'label':x, 'value':x} for x in df_guille]
+opt_guille = [{'label':'Less than 50K', 'value':0},
+              {'label':'More than 50K', 'value':1}]
 
 
 #References and descriptive text
 markdown_text = '''
 ### Description
 This app describes two datasets with interactive options for both of them.
-- Covid vaccinations: Information about country and date of your choice
-- Guille pon aqui tus cosas
-
+- Covid vaccinations: Information about country and date of your choice.
+- Adult Income: Data from and US Income Census, which classifies individuals in smaller or larger than US$ 50K anual income.
 ### References
-- [COVID-19 information](https://www.who.int/es/emergencies/diseases/novel-coronavirus-2019/question-and-answers-hub/q-a-detail/coronavirus-disease-covid-19)
-- [Original data from OurWorldInData](https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv)  
+- [COVID-19 information] (https://www.who.int/es/emergencies/diseases/novel-coronavirus-2019/question-and-answers-hub/q-a-detail/coronavirus-disease-covid-19)
+- [Original data from OurWorldInData] (https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv)  
+- [Original Adult Income dataset] (https://archive.ics.uci.edu/ml/datasets/Adult)
 '''
 
 #Covid table
 table_tab = dash_table.DataTable(
-                id='my-table',
-                columns=[{"name": i, "id": i} for i in df.columns]
-            )
+    id='my-table',
+    columns=[{"name": i, "id": i} for i in df.columns]
+)
+
+#Adult table
+table_tab2 = dash_table.DataTable(
+    id='my-table2',
+    columns=[{"name": i, "id": i} for i in data.columns]
+)
 
 #Covid graph
 graph_tab = dcc.Graph(id="my-graph")
@@ -94,27 +133,30 @@ tab_selected_style_Guille = {
 #define the app layout
 app.layout= html.Div([
     html.Div([html.H1(app.title, className="app-header--title")],
-        className= "app-header",
-    ),
-    html.Div([  
+             className= "app-header",
+             ),
+    html.Div([
         dcc.Markdown(markdown_text),
         html.Label(["Select countries/continents for Covid analysis:",
-            dcc.Dropdown('my-dropdown', options= opt_location, value= [opt_location[0]['value']], multi=True)
-        ]),
+                    dcc.Dropdown('my-dropdown', options= opt_location, value= [opt_location[0]['value']], multi=True)
+                    ]),
         html.Label(["Range of dates for Covid analysis:",
-                 dcc.RangeSlider(id="range",
-                     max= 10,
-                     min= 0,
-                     step= 1/10,
-                     marks= selected_dates,
-                     value= [0,1],
-                 )
-        ]),
+                    dcc.RangeSlider(id="range",
+                                    max= 10,
+                                    min= 0,
+                                    step= 1/10,
+                                    marks= selected_dates,
+                                    value= [0,1],
+                                    )
+                    ]),
         html.Div(id='data', style={'display': 'none'}),
         html.Div(id='dataRange', style={'display': 'none'}),
-        html.Label(["Select between 0 and 1 for Guille:",
-            dcc.Dropdown('guille-dropdown', options= opt_guille, value= [opt_guille[0]['value']], multi=False)
-        ]),
+        html.Label(["Select the income range(s):",
+                    dcc.Checklist(
+                        options=opt_guille,
+                        value=0
+                    )
+                    ]),
         dcc.Tabs(id="tabs", value='tab-t', children=[
             dcc.Tab(label='Table Covid', value='tab-t', style=tab_style_Arturo, selected_style=tab_selected_style_Arturo),
             dcc.Tab(label='Graph Covid', value='tab-g', style=tab_style_Arturo, selected_style=tab_selected_style_Arturo),
@@ -123,7 +165,7 @@ app.layout= html.Div([
         ]),
         html.Div(id='tabs-content')
     ],
-    className= "app-body")
+        className= "app-body")
 ])
 
 @app.callback(Output('tabs-content', 'children'),
@@ -136,9 +178,9 @@ def render_content(tab):
 
 
 @app.callback(
-     Output('my-table', 'data'),
-     Input('data', 'children'), 
-     State('tabs', 'value'))
+    Output('my-table', 'data'),
+    Input('data', 'children'),
+    State('tabs', 'value'))
 def update_table(data, tab):
     if tab != 'tab-t':
         return None
@@ -146,30 +188,30 @@ def update_table(data, tab):
     return dff.to_dict("records")
 
 @app.callback(
-     Output('my-graph', 'figure'),
-     Input('data', 'children'), 
-     State('tabs', 'value'))
+    Output('my-graph', 'figure'),
+    Input('data', 'children'),
+    State('tabs', 'value'))
 def update_graph(data, tab):
     if tab != 'tab-g':
         return None
     dff = pd.read_json(data, orient='split')
     return px.scatter(dff, x="date", y="total_vaccinations", color="location")
 
-@app.callback(Output('data', 'children'), 
-    Input('range', 'value'), 
-    State('my-dropdown', 'value'))
+@app.callback(Output('data', 'children'),
+              Input('range', 'value'),
+              State('my-dropdown', 'value'))
 def filter(range, values):
-     #filter by location given in values selector and in dates from range0 and range[1]
-     #keep in mind range is between 0 (start of df_dates) and 10 (end of df_dates)
+    #filter by location given in values selector and in dates from range0 and range[1]
+    #keep in mind range is between 0 (start of df_dates) and 10 (end of df_dates)
 
-     filter = df['location'].isin(values) & df['date'].between(df_dates[math.floor(range[0]*(nrows-1)/10)], df_dates[math.floor(range[1]*(nrows-1)/10)])
-     return df[filter].to_json(date_format='iso', orient='split')
+    filter = df['location'].isin(values) & df['date'].between(df_dates[math.floor(range[0]*(nrows-1)/10)], df_dates[math.floor(range[1]*(nrows-1)/10)])
+    return df[filter].to_json(date_format='iso', orient='split')
 
 
-@app.callback(Output('dataRange', 'children'), 
-    Input('my-dropdown', 'value'))
+@app.callback(Output('dataRange', 'children'),
+              Input('my-dropdown', 'value'))
 def dataRange(values):
-    filter = df['location'].isin(values) 
+    filter = df['location'].isin(values)
     dff = df[filter]
     dff_dates = dff['date'].sort_values().unique()
     min_dates = dff_dates[0]
